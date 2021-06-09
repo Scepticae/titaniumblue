cf00841067878eb8eb046d80cc7276ac3ead4b68
scepticae <samuel_leonard@comcast.net>
2021-06-09 10:47:24 -0400

    Added mobile responsiveness and menu icon

--- JS/index.js
+++ JS/index.js
@@ -1,21 +1,20 @@
-// const menuBtn = document.getElementById("menu-btn");
-// const nav = document.querySelector(".nav");
+const menuBtn = document.getElementById("menu-btn");
+const nav = document.querySelector(".nav");
 const logo = document.querySelector(".logo");
 const strips = document.querySelectorAll(".strip");
 
 removeActive();
 
-// menuBtn.addEventListener("click", () => {
-//   enav.classList.toggle("show");
-//   mnuBtn.classList.toggle("show");
-// });
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("show");
+  menuBtn.classList.toggle("show");
+});
 
 function displayPage() {
   logo.classList.add("active");
   strips.forEach((strip) => {
     strip.classList.add("active");
   });
-  // nav.classList.add("active");
 }
 
 function removeActive() {
@@ -24,6 +23,5 @@ function removeActive() {
     strip.classList.remove("active");
   });
 
-  // nav.classList.remove("active");
   setTimeout(displayPage, 100);
 }
